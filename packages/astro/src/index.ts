887bae9552493d9de3dfe3a97aa01130dd88dbb8
BlueNyang <xpsj20@gmail.com>
2026-03-09 01:09:29 +0900

    feat: include rollup setting and after build processing
    Signed-off-by: BlueNyang <xpsj20@gmail.com>

--- packages/astro/src/index.ts
+++ packages/astro/src/index.ts
@@ -1,10 +1,12 @@
 import type { AstroIntegration } from "astro";
 import { fileURLToPath } from "node:url";
 import { readFileSync, writeFileSync, existsSync } from "node:fs";
+import path from "node:path";
+import fs from "node:fs/promises";
 
 export default function tistoryPreviewer(): AstroIntegration {
   return {
-    name: "astro-tistory-skin-previewer",
+    name: "@tistory-skin-previewer/astro",
     hooks: {
       "astro:config:setup": ({
         command,
@@ -15,9 +17,7 @@ export default function tistoryPreviewer(): AstroIntegration {
         // injectScript,
       }) => {
         console.log("========= tistory-skin-previewer. By BlueNyang =========");
-        const skinEntry = fileURLToPath(
-          new URL("./src/pages/skin.astro", config.root)
-        );
+        const skinEntry = fileURLToPath(new URL("./src/pages/index.astro", config.root));
 
         // 개발 서버에서만 미들웨어와 가상 라우트 추가
         if (command === "dev") {
@@ -31,11 +31,7 @@ export default function tistoryPreviewer(): AstroIntegration {
               });
 
               if (frontmatterEndIndex > 0) {
-                lines.splice(
-                  frontmatterEndIndex,
-                  0,
-                  "export const prerender = false;"
-                );
+                lines.splice(frontmatterEndIndex, 0, "export const prerender = false;");
               } else {
                 lines.unshift("---");
                 lines.push("export const prerender = false;");
@@ -46,7 +42,7 @@ export default function tistoryPreviewer(): AstroIntegration {
               writeFileSync(skinEntry, modifiedContent, "utf-8");
 
               console.error(
-                "⚠️ skin.astro 파일에 'export const prerender = false;'를 추가했습니다."
+                "⚠️ skin.astro 파일에 'export const prerender = false;'를 추가했습니다.",
               );
             }
           }
@@ -72,13 +68,10 @@ export default function tistoryPreviewer(): AstroIntegration {
           // 빌드 시 "export const prerender = false;"가 있으면, 해당 라인을 제거
           if (existsSync(skinEntry)) {
             const skinContent = readFileSync(skinEntry, "utf-8");
-            const modifiedContent = skinContent.replace(
-              "export const prerender = false;",
-              ""
-            );
+            const modifiedContent = skinContent.replace("export const prerender = false;\n", "");
             writeFileSync(skinEntry, modifiedContent, "utf-8");
           } else {
-            console.error("⚠️ skin.astro 파일이 존재하지 않습니다.");
+            console.error("⚠️ index.astro 파일이 존재하지 않습니다.");
           }
         }
 
@@ -87,7 +80,14 @@ export default function tistoryPreviewer(): AstroIntegration {
           vite: {
             build: {
               rollupOptions: {
+                input: {
+                  common: "src/scripts/common.ts",
+                },
                 output: {
+                  entryFileNames: (assetInfo) => {
+                    if (assetInfo.name === "common") return "images/common.js";
+                    return "assets/[name]-[hash].js";
+                  },
                   // 3. CSS 파일을 'style.css' 단일 파일로 빌드
                   assetFileNames: (assetInfo) => {
                     if (assetInfo.name?.endsWith(".css")) {
@@ -103,6 +103,28 @@ export default function tistoryPreviewer(): AstroIntegration {
             base: command === "build" ? "./" : "/",
           },
         });
+      },
+
+      "astro:build:done": async ({ dir }) => {
+        const outDir = fileURLToPath(dir);
+        const indexFileName = "index.html";
+        const indexHtmlPath = path.join(outDir, indexFileName);
+        const skinPath = path.join(outDir, "skin.html");
+        try {
+          const html = await fs.readFile(indexHtmlPath, "utf-8");
+
+          const modifiedContent = html.replace(/href="\/style\.css"/g, 'href="./style.css"');
+
+          await fs.writeFile(skinPath, modifiedContent, "utf-8");
+          await fs.unlink(indexHtmlPath);
+
+          console.log("\n");
+          console.log("🎉 빌드가 완료되었습니다!");
+        } catch (err: any) {
+          if (err.code !== "ENOENT") {
+            console.error("❌ skin.html 파일 처리 중 오류 발생:", err);
+          }
+        }
       },
     },
   };
